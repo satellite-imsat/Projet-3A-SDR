ba1f13043f7c257424541fc5d76a163e03c960b9
Thomas Aussaguès <thomas.aussagues@imt-atlantique.net>
2022-10-24 08:56:26 +0200

    Removed useless functions

--- functions/mod_gmsk.py
+++ functions/mod_gmsk.py
@@ -19,46 +19,6 @@ def gaussian_filter(time_bandwidth_product : float, tb : float, up_sampling_fact
 
     return v_impulse_response
 
-def gmsk_mod(a,fc,L,BT,enable_plot=False):
-    """
-    Function to modulate a binary stream using GMSK modulation
-    Parameters:
-        BT : BT product (bandwidth x bit period) for GMSK
-        a : input binary data stream (0's and 1's) to modulate
-        fc : RF carrier frequency in Hertz
-        L : oversampling factor
-        enable_plot: True = plot transmitter waveforms (default False)
-    Returns:
-        (s_t,s_complex) : tuple containing the following variables
-            s_t : GMSK modulated signal with carrier s(t)
-            s_complex : baseband GMSK signal (I+jQ)
-    """
-    from scipy.signal import upfirdn,lfilter
-    
-    fs = L*fc; Ts=1/fs;Tb = L*Ts; # derived waveform timing parameters
-    a = 2 * a - 1
-    c_t = upfirdn(h=[1]*L, x= a, up = L) #NRZ pulse train c(t)
-    
-    k=1 # truncation length for Gaussian LPF
-    h_t = gaussian_filter(BT,Tb,L,k) # Gaussian LPF with BT=0.25
-    b_t = np.convolve(h_t,c_t,'full') # convolve c(t) with Gaussian LPF to get b(t)
-    bnorm_t = b_t/max(abs(b_t)) # normalize the output of Gaussian LPF to +/-1
-    
-    h = 0.5
-    # integrate to get phase information
-    phi_t = lfilter(b = [1], a = [1,-1], x = bnorm_t*Ts) * h*np.pi/Tb 
-    
-    I = np.cos(phi_t)
-    Q = np.sin(phi_t) # cross-correlated baseband I/Q signals
-  
-    s_complex = I - 1j*Q # complex baseband representation
-    t = Ts* np.arange(start = 0, stop = len(I)) # time base for RF carrier
-    sI_t = I*np.cos(2*np.pi*fc*t); sQ_t = Q*np.sin(2*np.pi*fc*t)
-    s_t = sI_t - sQ_t # s(t) - GMSK with RF carrier
-
-
-    return (s_t,s_complex)
-
 def mod_signal_gmsk(v_signal : np.ndarray, fc : float, up_sampling_factor : int, time_bandwidth_product : float, modulation_index = 0.5, filter_length = 1) -> np.ndarray:
 
     fs = up_sampling_factor * fc
@@ -119,19 +79,3 @@ def demod_gmsk(v_signal : np.ndarray, fc : float, up_sampling_factor : int) -> n
     #print('signal hat shape', signal_hat)
     return signal_hat
 
-def gmsk_demod(r_complex,L):
-    """
-    Function to demodulate a baseband GMSK signal
-    Parameters:
-        r_complex : received signal at receiver front end (complex form - I+jQ)
-        L : oversampling factor
-    Returns:
-        a_hat : detected binary stream
-    """
-    I=np.real(r_complex); Q = -np.imag(r_complex); # I,Q streams
-    z1 = Q * np.hstack((np.zeros(L), I[0:len(I)-L]))
-    z2 = I * np.hstack((np.zeros(L), Q[0:len(I)-L]))
-    z = z1 - z2
-    a_hat = (z[2*L-1:-L:L] > 0).astype(int) # sampling and hard decision
-    #sampling indices depend on the truncation length (k) of Gaussian LPF defined in the modulator
-    return a_hat
