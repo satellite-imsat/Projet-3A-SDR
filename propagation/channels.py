48c0f634b05874b09d758bc0df2b8212d6180939
Thomas Aussaguès <thomas.aussagues@imt-atlantique.net>
2022-12-27 16:34:42 +0100

    Added comments. Removed useless function.

--- propagation/channels.py
+++ propagation/channels.py
@@ -1,28 +1,6 @@
 import numpy as np
 
 
-
-def propagation_loss(v_signal : np.ndarray, tx_power_dB : float, g_tx_dBi, g_rx_dBi, carrier_frequency_Mhz : float, distance_km : float):
-
-    loss_dB = 32.45 + 20 * np.log10(carrier_frequency_Mhz) + 20 * np.log10(distance_km)
-    
-    rx_power_dB = tx_power_dB + g_tx_dBi + g_rx_dBi - loss_dB
-
-    rx_power_W = 10 ** (rx_power_dB / 10)
-    
-    # We transform the signal such that its power is rx_power_W
-    # We get the signal power
-    signal_power_dB = np.linalg.norm(v_signal) ** 2 / v_signal.shape[0]
-    signal_power_W = 10 ** (signal_power_dB / 10)
-
-    # We divide the signal by its power square root and multiply by the quare root of the desired one. 
-    # Remark 1 : note the dB -> W conversion.
-    # Remark 2 : we use the power square root since we work with the signal amplitude and not magnitude.
-    v_signal = v_signal * (rx_power_W / signal_power_W) ** 0.5
-
-    
-    return v_signal
-
 def awgn_channel(v_signal : np.ndarray, snr_db : float):
 
     '''Thif function simulates the propagation of a signal through an Additive White Gaussian Noise
